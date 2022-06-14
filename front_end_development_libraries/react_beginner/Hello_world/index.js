36f52d48391464a9c370b1b51467790ca56185ce
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-14 23:00:46 +0200

    show multiple elements on same reactDOM.render

---
+++ front_end_development_libraries/react_beginner/Hello_world/index.js
@@ -0,0 +1,9 @@
+// import ReactDOM from 'react-dom'
+// ReactDOM.render(<what>, <where>)
+
+ReactDOM.render(
+<ul>
+    <li>Flexible</li>
+    <li>Awesome</li>
+    <li>Fast</li>
+</ul>, document.getElementById('root'))
