69fc4d2ff551ee8e4fe4ea44a5791b5e7e78850b
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-15 23:00:24 +0200

    finished project

--- front_end_development_libraries/react_beginner/create_react_app/digital-business-card/src/components/Header.js
+++ front_end_development_libraries/react_beginner/create_react_app/digital-business-card/src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <header className="header">
             <img className="header__image" src={portrait} alt="woman"/>
             <div className="header__basic-info">
-                <h1>Joel Encinas</h1>
+                <h1>Joelle Encinas</h1>
                 <p>Junior Developer</p>
                 <a href="https://joelencinas.github.io/">Portfolio</a>
             </div>
