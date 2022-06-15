9a61d3b1559632e496fdb7e4ccdb14bf99a7fab3
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-16 01:18:07 +0200

    added images + navbar

--- front_end_development_libraries/react_beginner/data_driven_react/airbnb-experiences-clone/src/components/Navbar.js
+++ front_end_development_libraries/react_beginner/data_driven_react/airbnb-experiences-clone/src/components/Navbar.js
@@ -4,9 +4,7 @@ import airbnb_logo from "./../images/airbnb-logo.png"
 function Navbar() {
     return (
         <nav className="navbar">
-            <div className="navbar__content">
-                <img className="navbar__logo" src={airbnb_logo} alt="airbnb logo"/>
-            </div>
+            <img className="navbar__logo" src={airbnb_logo} alt="airbnb logo"/>
         </nav>
     )
 }
