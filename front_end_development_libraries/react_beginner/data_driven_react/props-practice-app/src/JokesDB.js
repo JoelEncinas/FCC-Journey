3c468ebac939de92d640058cf87f64aa9fb25a79
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-16 17:45:38 +0200

    moved jokes to external file

---
+++ front_end_development_libraries/react_beginner/data_driven_react/props-practice-app/src/JokesDB.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    setup : 'What\'s the best thing about Switzerland?',
+    punchline : 'I don\'t know, but the flag is a big plus.',
+    isPun : true
+  },
+  {
+    setup : 'I invented a new word!',
+    punchline : 'Plagiarism!',
+    isPun : true
+  },
+  {
+    setup : 'Why do we tell actors to “break a leg?”',
+    punchline : 'Because every play has a cast.',
+    isPun : true
+  },
+  {
+    setup : 'Hear about the new restaurant called Karma?',
+    punchline : 'There\'s no menu: You get what you deserve.',
+    isPun : true
+  },
+  {
+    setup : 'What do you call a joke that isn\'t funny? A sentence.',
+    isPun : false
+  }
+]
