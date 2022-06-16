fec9f85f0f981f5144d9655fb232aa02fd18a849
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-16 15:09:37 +0200

    small fixes

--- front_end_development_libraries/react_beginner/data_driven_react/props-practice-app/src/components/Joke.js
+++ front_end_development_libraries/react_beginner/data_driven_react/props-practice-app/src/components/Joke.js
@@ -3,16 +3,17 @@ import React from 'react';
 function Joke(props) {
     let displayValue = ''
 
-    if(props.punchline == null){
-        displayValue = 'none'
-    } else {
+    if(props.punchline){
         displayValue = 'block'
+    } else {
+        displayValue = 'none'
     }
 
     return (
         <div className='joke'>
             <h2 className='setup'>{props.setup}</h2>
             <p className='punchline' style={{display: displayValue}}>{props.punchline}</p>
+            <hr></hr>
         </div>
     )
 }
