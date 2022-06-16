ea72944b65819cad0ce22fe08f08f3c195c268d1
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-16 13:17:39 +0200

    props practice

--- front_end_development_libraries/react_beginner/data_driven_react/props-practice/index.js
+++ front_end_development_libraries/react_beginner/data_driven_react/props-practice/index.js
@@ -1,3 +1,25 @@
+import React from "react"
+import ReactDOM from "react-dowm"
+
+/*
 function addTwoNumbersTogether(a, b) {
     return a + b;
 }
+*/
+
+function App() {
+    const firstName = "Joe"
+    const lastName = "Schmoe"
+
+    const date = new Date()
+
+    return (
+        // curly braces for props
+        <div>
+            <h1>Hello {firstName} {lastName}</h1>
+            <p>It's around {date.getHours() % 12} o'clock!</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
