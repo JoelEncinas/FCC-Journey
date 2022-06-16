5416b15aa38ce850500e2d7e93f89b769f0c448a
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-16 18:02:42 +0200

    fixed images bug

--- front_end_development_libraries/react_beginner/data_driven_react/airbnb-experiences-clone/src/components/Card.js
+++ front_end_development_libraries/react_beginner/data_driven_react/airbnb-experiences-clone/src/components/Card.js
@@ -3,9 +3,9 @@ import React from "react";
 function Card(props) {
     return (
         <div className="card">
-            <img className="card__photo" src={`../images/${props.cardPhoto}`} alt="someone swimming"/>
+            <img className="card__photo" src={`./images/${props.cardPhoto}`} alt="someone swimming"/>
             <div className="rating">
-                <img className="rating__star" src={`../images/${props.star}`} alt="red star"/>
+                <img className="rating__star" src={`./images/${props.star}`} alt="red star"/>
                 <p className="rating__value">{props.ratingValue}</p>
                 <p className="rating__availability">({props.ratingAvailability}) • USA</p>
             </div>
