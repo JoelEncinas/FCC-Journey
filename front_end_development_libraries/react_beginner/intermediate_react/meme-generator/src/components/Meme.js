66769f962eb205db6c6d29e0abda54557a6fc572
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-19 20:11:46 +0200

    get url from data object on button click

--- front_end_development_libraries/react_beginner/intermediate_react/meme-generator/src/components/Meme.js
+++ front_end_development_libraries/react_beginner/intermediate_react/meme-generator/src/components/Meme.js
@@ -3,8 +3,9 @@ import image from "./../images/shut-up-and-take-my-money.jpg"
 import memesData from "../memesData"
 
 function Meme() {
-    function handleClick() {
-        console.log('click')
+    function getMemeImage() {
+        let random = Math.floor(Math.random() * memesData.data.memes.length)
+        console.log(memesData.data.memes[random].url)
     }
 
     function handleMouseEnter(){
@@ -18,7 +19,7 @@ function Meme() {
                     <input className="meme__input" type='text' placeholder="Top text"></input>
                     <input className="meme__input" type='text' placeholder="Bottom text"></input>
                 </div>
-                <button className="meme__input-submit" onClick={handleClick}>Generate meme</button>
+                <button className="meme__input-submit" onClick={getMemeImage}>Generate meme</button>
                 <img className="meme__image" onMouseEnter={handleMouseEnter} src={image} alt="meme"/>
             </div>
         </main>
