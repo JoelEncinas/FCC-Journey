152fa5241a7f3d6f24b183153b885d26d0035f2f
JoelEncinas <joelencinasmartin@gmail.com>
2022-06-20 00:04:23 +0200

    added state to show boxes

--- front_end_development_libraries/react_beginner/intermediate_react/box-challenge/src/App.js
+++ front_end_development_libraries/react_beginner/intermediate_react/box-challenge/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import boxes from './boxes'
 
+/**
+ * Challenge part 1:
+ * 1. Initialize state with the default value of the
+ *    array pulled in from boxes.js
+ * 2. Map over that state array and display each one
+ *    as an empty square (black border, transparent bg color)
+ *    (Don't worry about using the "on" property yet)
+ */
+
 function App() {
+  const [boxesArray, changeState] = React.useState(boxes)
+
+  let boxesElements = boxesArray.map(box => {
+    return <div className='box' key={box.id}></div>
+  })
+
   return (
     <main>
-        <h1>Boxes will go here</h1>
+        <h1>Boxes</h1>
+        <div className='box-container'>
+          {boxesElements}
+        </div>
     </main>
-)
+  )
 }
 
 export default App;
