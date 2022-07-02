8af61992cec3018b6b7acfa2d8ad2169a6d23a71
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-02 14:55:58 +0200

    controlled inputs

--- front_end_development_libraries/Extra_projects/react_beginner/forms/forms-practice/src/Form.js
+++ front_end_development_libraries/Extra_projects/react_beginner/forms/forms-practice/src/Form.js
@@ -4,25 +4,44 @@ export default function Form() {
   const [formData, setFormData] = React.useState({
     firstName: "",
     lastName: "",
-    email: ""
+    email: "",
   });
 
   console.log(formData);
 
   function handleChange(event) {
-    setFormData(prevFormData => {
-        return {
-            ...prevFormData, 
-            [event.target.name]: event.target.value
-        }
-    })
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
   }
 
   return (
     <form>
-      <input type="text" placeholder="First Name" name="firstName" onChange={handleChange} />
-      <input type="text" placeholder="Last Name" name="lastName" onChange={handleChange} />
-      <input type="email" placeholder="jhon@mail.com" name="email" onChange={handleChange} />
+      {/* Controlled components by setting value equal to the state */}
+      <input
+        type="text"
+        placeholder="First Name"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        placeholder="jhon@mail.com"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+      />
     </form>
   );
 }
