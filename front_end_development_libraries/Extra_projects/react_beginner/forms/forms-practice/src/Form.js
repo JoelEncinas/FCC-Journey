da3fa7017dfdcaa670794b94729a75b8e58bbe00
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-02 14:38:35 +0200

    handle input changes and use object in state

--- front_end_development_libraries/Extra_projects/react_beginner/forms/forms-practice/src/Form.js
+++ front_end_development_libraries/Extra_projects/react_beginner/forms/forms-practice/src/Form.js
@@ -1,23 +1,26 @@
 import React from "react";
 
 export default function Form() {
-  const [userInfo, setName] = React.useState([,]);
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+  });
 
-  console.log(userInfo);
+  console.log(formData);
 
   function handleChange(event) {
-    if (event.target.placeholder === "First Name") {
-      setName([userInfo[0], event.target.value]);
-    } else {
-      setName([event.target.value, userInfo[1]]);
-    }
-    // console.log(event.target.value);
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData, 
+            [event.target.name]: event.target.value
+        }
+    })
   }
 
   return (
     <form>
-      <input type="text" placeholder="First Name" onChange={handleChange} />
-      <input type="text" placeholder="Last Name" onChange={handleChange} />
+      <input type="text" placeholder="First Name" name="firstName" onChange={handleChange} />
+      <input type="text" placeholder="Last Name" name="lastName" onChange={handleChange} />
     </form>
   );
 }
