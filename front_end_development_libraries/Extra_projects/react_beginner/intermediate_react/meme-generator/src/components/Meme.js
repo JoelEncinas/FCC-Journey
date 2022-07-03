9875febb9925835b2f0d11c59a3f387241dcb4c3
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-03 21:09:08 +0200

    change meme text as state

--- front_end_development_libraries/Extra_projects/react_beginner/intermediate_react/meme-generator/src/components/Meme.js
+++ front_end_development_libraries/Extra_projects/react_beginner/intermediate_react/meme-generator/src/components/Meme.js
@@ -1,31 +1,42 @@
-import React from "react"
-import image from "./../images/shut-up-and-take-my-money.jpg"
-import memesData from "../memesData"
+import React from "react";
+import image from "./../images/shut-up-and-take-my-money.jpg";
+import memesData from "../memesData";
 
 function Meme() {
-    const [meme, setMeme] = React.useState({
-        image: image, 
-        top: '', 
-        bot: ''
-    })
+  const [meme, setMeme] = React.useState({
+    image: image,
+    top: "",
+    bot: "",
+  });
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
-    function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        let random = Math.floor(Math.random() * memesData.data.memes.length)
-        const url = memesArray[random].url
-        setMeme(prevMeme => ({
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    let random = Math.floor(Math.random() * memesData.data.memes.length);
+    const url = memesArray[random].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      image: url,
+    }));
+  }
+
+  function handleMouseEnter() {
+    // console.log('entered img')
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setMeme((prevMeme) => {
+        return{
             ...prevMeme,
-            image: url
-        }))
-    }
+            [name]: value
+        }
+    }) 
+  }
 
-    function handleMouseEnter(){
-        // console.log('entered img')
-    }
-
-    /* CHALLENGE
+  /* CHALLENGE
     let thingsArray = ['Thing 1']
 
     function addItemToArray(){
@@ -35,24 +46,45 @@ function Meme() {
     }
     */
 
-    return (
-        <div>
-            <main className="meme">
-                <div className="meme__form">
-                    <div className="meme__inputs">
-                        <input className="meme__input" type='text' placeholder="Top text"></input>
-                        <input className="meme__input" type='text' placeholder="Bottom text"></input>
-                    </div>
-                    <button className="meme__input-submit" onClick={getMemeImage}>Generate meme</button>
-                    <div className="meme__image-container">
-                    <img className="meme__image" onMouseEnter={handleMouseEnter} src={meme.image} alt="meme"/>
-                    <h2 className="meme__text top">One does not simply</h2>
-                    <h2 className="meme__text bottom">Walk into mordor</h2>
-                    </div>
-                </div>
-            </main>
+  return (
+    <div>
+      <main className="meme">
+        <div className="meme__form">
+          <div className="meme__inputs">
+            <input
+              value={meme.top}
+              name="top"
+              onChange={handleChange}
+              className="meme__input"
+              type="text"
+              placeholder="Top text"
+            ></input>
+            <input
+              value={meme.bot}
+              name="bot"
+              onChange={handleChange}
+              className="meme__input"
+              type="text"
+              placeholder="Bottom text"
+            ></input>
+          </div>
+          <button className="meme__input-submit" onClick={getMemeImage}>
+            Generate meme
+          </button>
+          <div className="meme__image-container">
+            <img
+              className="meme__image"
+              onMouseEnter={handleMouseEnter}
+              src={meme.image}
+              alt="meme"
+            />
+            <h2 className="meme__text top">{meme.top}</h2>
+            <h2 className="meme__text bottom">{meme.bot}</h2>
+          </div>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
 
-export default Meme
+export default Meme;
