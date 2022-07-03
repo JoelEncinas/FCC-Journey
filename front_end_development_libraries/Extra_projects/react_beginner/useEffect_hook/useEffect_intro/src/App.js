2440f8e4364e0f6c699ff9d3d36a0ac7387402d5
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-03 22:11:13 +0200

    dependencies array

--- front_end_development_libraries/Extra_projects/react_beginner/useEffect_hook/useEffect_intro/src/App.js
+++ front_end_development_libraries/Extra_projects/react_beginner/useEffect_hook/useEffect_intro/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  const [starWarsData, setStarsWarsData] = React.useState({})
+  const [starWarsData, setStarsWarsData] = React.useState({});
 
   console.log("rendered");
 
   // side effects
-  React.useEffect(function() {
+  React.useEffect(function () {
     fetch("https://swapi.dev/api/people/1")
-    .then(res => res.json())
-    .then(data => setStarsWarsData(data))
-  }, )
+      .then((res) => res.json())
+      .then((data) => setStarsWarsData(data));
+    // dependencies arrays
+    // if array empty it only runs 1 time when component rendered
+  }, []);
+
+  // React.useEffect(() => {
+  //   console.log("Effect function run");
+  // }, [count])
 
   return (
     <div className="App">
