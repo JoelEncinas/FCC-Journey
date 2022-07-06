76973c6ebf8ebcb2eb661efa40576d23e535b7aa
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-06 23:24:56 +0200

    call api to get new character on state update

--- front_end_development_libraries/Extra_projects/react_beginner/useEffect_hook/useEffect_intro/src/App.js
+++ front_end_development_libraries/Extra_projects/react_beginner/useEffect_hook/useEffect_intro/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function App() {
   const [starWarsData, setStarsWarsData] = React.useState({});
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(1);
 
   console.log("rendered");
 
   // side effects
   React.useEffect(function () {
-    fetch("https://swapi.dev/api/people/1")
+    fetch(`https://swapi.dev/api/people/${count}`)
       .then((res) => res.json())
       .then((data) => setStarsWarsData(data));
     // dependencies arrays
@@ -19,8 +19,16 @@ function App() {
   //   console.log("Effect function run");
   // }, [count])
 
+  function getNewCharacter(){
+    setCount((prevValue) => {
+      return prevValue + 1;
+    })
+  }
+
   return (
     <div className="App">
+      <h2>The count is {count}</h2>
+      <button onClick={getNewCharacter}>Get next character</button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
