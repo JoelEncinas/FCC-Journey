5aa8de00a07e00709fd9be8858bf0f810b23e97e
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-07 19:01:35 +0200

    created window tracker base app

---
+++ front_end_development_libraries/Extra_projects/react_beginner/useEffect_hook/screen_width_state/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WindowTracker from "./WindowTracker"
+
+function App() {
+  return (
+    <div className="App">
+      <button>
+        Toggle WindowTracker
+      </button>
+      <WindowTracker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front_end_development_libraries/Extra_projects/react_beginner/useEffect_hook/screen_width_state/src/WindowTracker.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WindowTracker(){
+    return (
+        <div>
+            <h1>Window width: {window.innerWidth}</h1>
+        </div>
+    )
+}
+
+export default WindowTracker;
