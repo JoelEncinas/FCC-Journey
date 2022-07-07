ee5ec5ed850268df33823ccd281a0c636b006915
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-07 19:31:50 +0200

    display window width with useEffect

--- front_end_development_libraries/Extra_projects/react_beginner/useEffect_hook/screen_width_state/src/WindowTracker.js
+++ front_end_development_libraries/Extra_projects/react_beginner/useEffect_hook/screen_width_state/src/WindowTracker.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 function WindowTracker(){
+
+    const [windowWidth, windowWidthChange] = React.useState(window.innerWidth)
+
+    React.useEffect(() => {
+        window.addEventListener("resize", function() {
+            windowWidthChange(window.innerWidth)
+        })
+        console.log('resized');
+    }, [])
+
     return (
         <div>
-            <h1>Window width: {window.innerWidth}</h1>
+            <h1>Window width: {windowWidth}</h1>
         </div>
     )
 }
