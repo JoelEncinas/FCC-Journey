7284a96f5a2231d06f65cbf48414b9da66d4e567
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-08 12:46:19 +0200

    added styles for button

--- front_end_development_libraries/Extra_projects/react_beginner/create_react_app/react-facts-v2/src/components/Navbar.js
+++ front_end_development_libraries/Extra_projects/react_beginner/create_react_app/react-facts-v2/src/components/Navbar.js
@@ -3,7 +3,7 @@ import logo from "./../images/logo-react.png";
 
 export default function Navbar(props) {
   return (
-    <nav className="nav">
+    <nav className={props.darkMode ? "dark": ""}>
       <div className="logo">
         <img className="logo__image" src={logo} alt="react logo" />
         <h2 className="logo__title">ReactFacts</h2>
