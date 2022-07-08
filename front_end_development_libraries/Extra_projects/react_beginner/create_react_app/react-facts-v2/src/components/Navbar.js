01b8d70154c3cf4c6ebd686bc583f32219088632
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-08 12:46:11 +0200

    added scroll button

--- front_end_development_libraries/Extra_projects/react_beginner/create_react_app/react-facts-v2/src/components/Navbar.js
+++ front_end_development_libraries/Extra_projects/react_beginner/create_react_app/react-facts-v2/src/components/Navbar.js
@@ -1,16 +1,20 @@
-import React from "react"
-import logo from './../images/logo-react.png'
+import React from "react";
+import logo from "./../images/logo-react.png";
 
-export default function Navbar() {
-    return (
-        <nav className="nav">
-            <div className="logo">
-                <img className="logo__image" src={logo} alt="react logo"/>
-                <h2 className="logo__title">ReactFacts</h2>
-            </div>
-            <div className="index">
-                <p>React Course - Project 1</p>
-            </div>
-        </nav>
-    )
+export default function Navbar(props) {
+  return (
+    <nav className="nav">
+      <div className="logo">
+        <img className="logo__image" src={logo} alt="react logo" />
+        <h2 className="logo__title">ReactFacts</h2>
+      </div>
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
+    </nav>
+  );
 }
