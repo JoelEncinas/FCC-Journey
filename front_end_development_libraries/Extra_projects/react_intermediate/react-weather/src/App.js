3d893939aba5f6293f53bde742cc4e3c5fb744fb
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-15 01:32:23 +0200

    created search component with async paginate

--- front_end_development_libraries/Extra_projects/react_intermediate/react-weather/src/App.js
+++ front_end_development_libraries/Extra_projects/react_intermediate/react-weather/src/App.js
@@ -1,9 +1,13 @@
 import Search from './components/search/search'
 
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
+
   return (
-    <div className="App">
-      <Search />
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
