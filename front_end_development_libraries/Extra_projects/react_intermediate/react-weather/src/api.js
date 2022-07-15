751b0badcc89d9c2ff0587f82a456863c707deed
JoelEncinas <joelencinasmartin@gmail.com>
2022-07-15 16:45:25 +0200

    added api call and added them to asyncpaginate

---
+++ front_end_development_libraries/Extra_projects/react_intermediate/react-weather/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "775e949c42mshcd6b696ad7f6acfp1aa502jsndd57d4df9d91",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- front_end_development_libraries/Extra_projects/react_intermediate/react-weather/src/components/search/search.js
+++ front_end_development_libraries/Extra_projects/react_intermediate/react-weather/src/components/search/search.js
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
-    onSearchChange(searchData)
-  }
+    onSearchChange(searchData);
+  };
 
   return (
     <AsyncPaginate
