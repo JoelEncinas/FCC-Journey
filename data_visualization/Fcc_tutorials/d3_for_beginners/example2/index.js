1442dbe03c813a134767d9d6b954d2923cada049
Joel Encinas <joelencinasmartin@gmail.com>
2023-01-28 18:11:07 +0100

    added scales

--- data_visualization/Fcc_tutorials/d3_for_beginners/example2/index.js
+++ data_visualization/Fcc_tutorials/d3_for_beginners/example2/index.js
@@ -1,4 +1,5 @@
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+// var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var dataset = [2, 3, 1, 4, 5];
 
 var svgWidth = 500;
 var svgHeight = 300;
@@ -10,12 +11,16 @@ var svg = d3.select('svg')
     .attr('height', svgHeight)
     .style('background-color', 'lightblue');
 
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgHeight]);
+
 var barChart = svg.selectAll('rect')
     .data(dataset)
     .enter()
-    .append('rect')
-    .attr('y', (d) => svgHeight - d)
-    .attr('height', (d) => d)
+    .append('rect')  
+    .attr('y', (d) => svgHeight - yScale(d))
+    .attr('height', (d) => yScale(d))
     .attr('width', barWidth - barPadding)
     .attr('transform', (d, i) => {
         var translate = [barWidth * i, 0];
