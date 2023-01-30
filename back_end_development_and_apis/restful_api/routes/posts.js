5d634ff650f3b1764b1031779ef6ede5165773cb
Joel Encinas <joelencinasmartin@gmail.com>
2023-01-30 11:49:41 +0100

    get all posts and get post by id

--- back_end_development_and_apis/restful_api/routes/posts.js
+++ back_end_development_and_apis/restful_api/routes/posts.js
@@ -2,25 +2,39 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-// routes
-router.get('/', (req, res) => {
-    res.send('We are on posts');
-});
+// get back all posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err});
+    }
+}); 
 
-
-router.post('/', (req, res) => {
+// submit post
+router.post('/', async (req, res) => {
     const post = new Post({
         title:  req.body.title,
         description: req.body.description
     });
 
-    post.save()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json({message: err});
-        })
+    try  {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.json({ message: err});
+    }
 });
+
+// get post by id
+router.get('/:postId', async (req, res) => {
+    try  {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err});
+    }
+})
 
 module.exports = router;
