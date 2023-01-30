03a15cc743be9ea4ed84ef4610f79202b0e6599a
Joel Encinas <joelencinasmartin@gmail.com>
2023-01-30 08:45:03 +0100

    define post schema

---
+++ back_end_development_and_apis/restful_api/models/Post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mmongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
