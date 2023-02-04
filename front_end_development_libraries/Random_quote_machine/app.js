2615653539506d34302461e364536096d7cfa778
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-04 10:41:49 +0100

    finished final touches

--- front_end_development_libraries/Random_quote_machine/app.js
+++ front_end_development_libraries/Random_quote_machine/app.js
@@ -1,56 +1,58 @@
+// using local json
+const QUOTES_URL = "./quotes.json";
+const TWITTER_URL = "https://twitter.com/intent/tweet";
+
+// page elements
+const quoteText = $("#text");
+const quoteAuthor = $("#author");
+const newQuoteBtn = $("#new-quote");
+const tweetQuoteBtn = $("#tweet-quote");
+const container = $("#main-container");
+const body = $("body");
+
+// get quote logic
+function getNewQuote() {
+  $.getJSON(QUOTES_URL, function (data) {
+    // generate random number and search quote
+    let randomIndex = Math.floor(Math.random() * data.quotes.length);
+    let randomQuote = data.quotes[randomIndex];
+
+    // clear elements
+    quoteText.empty();
+    quoteAuthor.empty();
+
+    // append data
+    quoteText.append('" ' + randomQuote.quote + ' "');
+    quoteAuthor.append("- " + randomQuote.author);
+
+    // add twitter link
+    tweetQuoteBtn.attr(
+      "href",
+      TWITTER_URL +
+        `?hashtags=quotes&text="${randomQuote.quote}" - ${randomQuote.author}`
+    );
+
+    // change theme
+    let color = changeBackgroundColor();
+    container.css({ "background-color": color });
+    body.css({ "background-color": color });
+    quoteText.css({ color: color });
+    quoteAuthor.css({ color: color });
+    newQuoteBtn.css({ color: color });
+    tweetQuoteBtn.css({ color: color });
+  });
+}
+
+// change background color on new quote request
+function changeBackgroundColor() {
+  let randomIndex = Math.floor(Math.random() * paleColors.length);
+  let randomColor = paleColors[randomIndex];
+
+  return randomColor;
+}
+
+// when document loads initialize logic
 $(document).ready(function () {
-  // using local json
-  const QUOTES_URL = "./quotes.json";
-  const TWITTER_URL = "https://twitter.com/intent/tweet";
-
-  // page elements
-  const quoteBox = $("#quote-box");
-  const quoteText = $("#text");
-  const quoteAuthor = $("#author");
-  const newQuoteBtn = $("#new-quote");
-  const tweetQuoteBtn = $("#tweet-quote");
-  const container = $(".container");
-  const body = $("body");
-
-  // get quote logic
-  function getNewQuote() {
-    $.getJSON(QUOTES_URL, function (data) {
-      // generate random number and search quote
-      let randomIndex = Math.floor(Math.random() * data.quotes.length);
-      let randomQuote = data.quotes[randomIndex];
-
-      // clear elements
-      quoteText.empty();
-      quoteAuthor.empty();
-
-      // append data
-      quoteText.append("\"" + randomQuote.quote + "\"");
-      quoteAuthor.append(randomQuote.author);
-
-      // add twitter link
-      tweetQuoteBtn.attr(
-        "href",
-        TWITTER_URL +
-          `?hashtags=quotes&text="${randomQuote.quote}" - ${randomQuote.author}`
-      );
-
-      // change theme
-      let color = changeBackgroundColor();
-      container.css({ "background-color": color});
-      body.css({ "background-color": color });
-      quoteText.css({ color: color });
-      quoteAuthor.css({ color: color });
-    });
-  }
-
-  // change background color on new quote request
-  function changeBackgroundColor() {
-    let randomIndex = Math.floor(Math.random() * paleColors.length);
-    let randomColor = paleColors[randomIndex];
-
-    return randomColor;
-  }
-
   // get quote on first load
   getNewQuote();
 
