97ce294932e9b15a26346092199887aba6bbc4e4
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-04 13:44:28 +0100

    styles and extra functionality completed

--- front_end_development_libraries/Markdown_previewer/app.js
+++ front_end_development_libraries/Markdown_previewer/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   const editor = $("#editor");
   const preview = $("#preview");
+  const clearBtn = $("#clearBtn");
+  const copyBtn = $("#copyBtn");
 
-  let markdown = "# Welcome to the markdown previewer... \n# Header (H1)\n\n## Subheader (H2)\n\n[Link](https://www.example.com)\n\nInline code: `console.log('Hello World!')`\n\nCode block:\n```\nfunction greet() {\n  console.log('Hello World!');\n}\n```\n\n- List item 1\n- List item 2\n\n> Blockquote\n\n![Image Alt Text](https://picsum.photos/200)\n\n**Bolded Text**";
+  let markdown = "# Header (H1)\n\n## Subheader (H2)\n\n[Link](https://www.example.com)\n\nInline code: `console.log('Hello World!')`\n\nCode block:\n```\nfunction greet() {\n  console.log('Hello World!');\n}\n```\n\n- List item 1\n- List item 2\n\n> Blockquote\n\n![Image Alt Text](https://picsum.photos/200)\n\n**Bolded Text**";
 
   editor.val(markdown);
   preview.html(marked(markdown, { breaks: true }));
@@ -11,5 +13,19 @@ $(document).ready(function() {
     var markdown = editor.val();
     var html = marked(markdown, { breaks: true });
     preview.html(html);
+  });
+
+  clearBtn.on("click", function() {
+    editor.val("");
+    preview.html("");
+  });
+
+  copyBtn.on("click", function() {
+    const html = preview.html();
+    const tempInput = $("<input>");
+    $("body").append(tempInput);
+    tempInput.val(html).select();
+    document.execCommand("copy");
+    tempInput.remove();
   });
 });
