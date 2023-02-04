054564aa7e0abf33c31eb17532088ac9c45c010e
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-04 09:45:30 +0100

    added color change on get quote

--- front_end_development_libraries/Random_quote_machine/app.js
+++ front_end_development_libraries/Random_quote_machine/app.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
   const quoteAuthor = $("#author");
   const newQuoteBtn = $("#new-quote");
   const tweetQuoteBtn = $("#tweet-quote");
+  const container = $(".container");
+  const body = $("body");
 
   // get quote logic
   function getNewQuote() {
@@ -22,14 +24,31 @@ $(document).ready(function () {
       quoteAuthor.empty();
 
       // append data
-      quoteText.append(randomQuote.quote);
+      quoteText.append("\"" + randomQuote.quote + "\"");
       quoteAuthor.append(randomQuote.author);
 
+      // add twitter link
       tweetQuoteBtn.attr(
         "href",
-        TWITTER_URL + `?hashtags=quotes&text="${randomQuote.quote}" - ${randomQuote.author}`
+        TWITTER_URL +
+          `?hashtags=quotes&text="${randomQuote.quote}" - ${randomQuote.author}`
       );
+
+      // change theme
+      let color = changeBackgroundColor();
+      container.css({ "background-color": color});
+      body.css({ "background-color": color });
+      quoteText.css({ color: color });
+      quoteAuthor.css({ color: color });
     });
+  }
+
+  // change background color on new quote request
+  function changeBackgroundColor() {
+    let randomIndex = Math.floor(Math.random() * paleColors.length);
+    let randomColor = paleColors[randomIndex];
+
+    return randomColor;
   }
 
   // get quote on first load
