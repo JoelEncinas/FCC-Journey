6afa50237c1ea39c0310775c3499c1acb4b59ef7
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-15 21:15:01 +0100

    basic logic done

--- scientific_computing_with_python/Arithmetic_formatter/main.py
+++ scientific_computing_with_python/Arithmetic_formatter/main.py
@@ -1,4 +1,4 @@
-def arithmetic_arranger(problems, solutions = False):
+def arithmetic_arranger(problems, solution=False):
     # err situations
     if len(problems) > 5:
         return 'Error: Too many problems.'
@@ -6,13 +6,41 @@ def arithmetic_arranger(problems, solutions = False):
     for i in range(len(problems)):
         if not problems[i].split(" ")[0].isdigit() or not problems[i].split(" ")[2].isdigit():
             return "Error: Numbers must only contain digits."
-        
+
         if len(str(problems[i].split(" ")[0])) > 4 or len(str(problems[i].split(" ")[2])) > 4:
             return "Error: Numbers cannot be more than four digits."
-        
+
         if problems[i].split(" ")[1] not in ['+', '-']:
-            return "Error: Operator must be '+' or '-'."        
+            return "Error: Operator must be '+' or '-'."
 
-    #return arranged_problems
+    # conversion
+    arranged_problems = ""
+    space = " "
+    dash = "-"
 
-print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 + 49"], True))
+    for i in range(len(problems)):
+        num1 = problems[i].split(" ")[0]
+        num2 = problems[i].split(" ")[2]
+        operand = problems[i].split(" ")[1]
+        num1Length = len(str(num1))
+        num2Length = len(str(num2))
+        shortest = num1Length if len(str(num1Length)) > len(
+            str(num2Length)) else num2Length
+        longest = num2Length if len(str(num2Length)) > len(
+            str(num1Length)) else num1Length
+        arranged_problems += f"{space * 2}{num1}\n"
+        arranged_problems += f"{operand}{space}{space * (longest - shortest)}{num2}\n"
+        arranged_problems += f"{dash * (longest + 2)}\n"
+
+        if (solution == True):
+            if operand == "+":
+                solutionNumber = int(num1) + int(num2)
+            else:
+                solutionNumber = int(num1) - int(num2)
+
+            arranged_problems += f"{space * 2}{solutionNumber}"
+
+    return arranged_problems
+
+
+print(arithmetic_arranger(["32 + 8"], True))
