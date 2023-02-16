9dc2ffb6dee10565895bc66a9f68e56f413bb03a
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-16 17:58:03 +0100

    created class and deposit & withdraw methods

--- scientific_computing_with_python/Budget_app/main.py
+++ scientific_computing_with_python/Budget_app/main.py
@@ -1,8 +1,30 @@
 class Category:
-    name = ''
-    def __init__(self, name):
-        self.name = name
+    category = ''
+    ledger = []
 
-q = Category('rock')
-q.name = 'pop'
-print(q.name)
+    def __init__(self, category):
+        self.category = category
+    
+    def deposit(self, amount, description = ""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def get_all_amounts(self):
+        amounts = 0
+        for entry in self.ledger:
+            amounts += entry['amount']
+        return amounts
+
+    def withdraw(self, amount, description = ""):
+        if amount > self.get_all_amounts():
+            return False
+        else:
+            self.ledger.append({"amount": -1 * amount, "description": description})
+            return True
+        
+a = Category("food")
+a.deposit(10, "bananas")
+a.deposit(5, "beer")
+print(a.withdraw(60, "skate"))
+print(a.withdraw(3, "chocolate"))
+print(a.get_all_amounts())
+print(a.ledger)
