221213af2bcd5fc727689776b9d9b9a8231d24c9
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-16 18:23:04 +0100

    created checkfunds methos

--- scientific_computing_with_python/Budget_app/main.py
+++ scientific_computing_with_python/Budget_app/main.py
@@ -1,43 +1,56 @@
 class Category:
-    category = ''
+    category = ""
     ledger = []
 
     def __init__(self, category):
         self.category = category
-    
-    def deposit(self, amount, description = ""):
+
+    def deposit(self, amount, description=""):
         self.ledger.append({"amount": amount, "description": description})
 
     def get_balance(self):
         total_amount = 0
+        
         for entry in self.ledger:
             total_amount += entry['amount']
         return total_amount
 
-    def withdraw(self, amount, description = ""):
+    def withdraw(self, amount, description=""):
         if amount > self.get_balance():
             return False
         else:
-            self.ledger.append({"amount": -1 * amount, "description": description})
+            self.ledger.append(
+                {"amount": -1 * amount, "description": description})
             return True
-    
+
     def transfer(self, amount, budget_category):
         if amount < self.get_balance() and budget_category.get_balance() >= amount:
             self.withdraw(amount, f"Transfer to {budget_category.category}")
+            budget_category.deposit(amount, f"Transfer from {self.category}")
             return True
         else:
             return False
+
+    def check_funds(self, amount):
+        total_amount = 0
+
+        for entry in self.ledger:
+            total_amount += entry['amount']
+
+        if amount > total_amount:
+            return False
+        else:
+            return True
+
 
 food = Category("Food")
 food.deposit(10, "check")
 food.withdraw(5, "beer")
 food.withdraw(20, "cake")
 clothing = Category("Clothing")
+clothing.deposit(20, "check")
 food.transfer(6, clothing)
 food.transfer(2, clothing)
 
-print(food.ledger)
 print(food.get_balance())
-
-print(clothing.ledger)
 print(clothing.get_balance())
