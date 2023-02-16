66eeecebedbd38692c63529806f6167eefcf023f
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-16 23:47:28 +0100

    first push

---
+++ front_end_development_libraries/Javascript_calculator/app.js
@@ -0,0 +1,8 @@
+import React from 'https://esm.sh/react@18.2.0'
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0'
+
+const App = () => {
+  return <div>Hello World</div>
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
