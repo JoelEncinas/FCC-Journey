711dc888980e3d10a6c9b35ccf08dd25c1e1a259
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-16 18:45:30 +0100

    first part of the challenge done, class created

--- scientific_computing_with_python/Budget_app/main.py
+++ scientific_computing_with_python/Budget_app/main.py
@@ -1,22 +1,20 @@
 class Category:
-    category = ""
-    ledger = []
-
-    def __init__(self, category):
-        self.category = category
+    def __init__(self, budget_category):
+        self.instance_category = budget_category
+        self.ledger = []
 
     def deposit(self, amount, description=""):
         self.ledger.append({"amount": amount, "description": description})
 
     def get_balance(self):
         total_amount = 0
-        
+
         for entry in self.ledger:
             total_amount += entry['amount']
         return total_amount
 
     def withdraw(self, amount, description=""):
-        if amount > self.get_balance():
+        if not self.check_funds(amount):
             return False
         else:
             self.ledger.append(
@@ -24,18 +22,15 @@ class Category:
             return True
 
     def transfer(self, amount, budget_category):
-        if amount < self.get_balance() and budget_category.get_balance() >= amount:
-            self.withdraw(amount, f"Transfer to {budget_category.category}")
-            budget_category.deposit(amount, f"Transfer from {self.category}")
+        if self.check_funds(amount) and budget_category.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {budget_category.instance_category}")
+            budget_category.deposit(amount, f"Transfer from {self.instance_category}")
             return True
         else:
             return False
 
     def check_funds(self, amount):
-        total_amount = 0
-
-        for entry in self.ledger:
-            total_amount += entry['amount']
+        total_amount = self.get_balance()
 
         if amount > total_amount:
             return False
@@ -44,13 +39,26 @@ class Category:
 
 
 food = Category("Food")
+print(food)
 food.deposit(10, "check")
-food.withdraw(5, "beer")
-food.withdraw(20, "cake")
-clothing = Category("Clothing")
-clothing.deposit(20, "check")
-food.transfer(6, clothing)
-food.transfer(2, clothing)
+print(food.get_balance())
+print(food.withdraw(5, "beer"))
+print(food.get_balance())
+print(food.withdraw(20, "cake"))
+print(food.get_balance())
+print(food.ledger)
 
 print(food.get_balance())
-print(clothing.get_balance())
+
+clothes = Category("Clothing")
+print(clothes)
+clothes.deposit(50, "check")
+print(clothes.get_balance())
+print(clothes.ledger)
+
+print(food.transfer(10, clothes))
+print(food.transfer(5, clothes))
+print(food.ledger)
+print(food.get_balance())
+print(clothes.ledger)
+print(clothes.get_balance())
