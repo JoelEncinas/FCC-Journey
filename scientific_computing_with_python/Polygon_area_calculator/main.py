c485d979b36be7524b9eb42e381076d4fcefa7c6
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-17 13:05:42 +0100

    rectangle class done

--- scientific_computing_with_python/Polygon_area_calculator/main.py
+++ scientific_computing_with_python/Polygon_area_calculator/main.py
@@ -6,9 +6,36 @@ class Rectangle:
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
 
+    def set_width(self, width):
+        self.width = width
     
+    def set_height(self, height):
+        self.height = height
 
-a = Rectangle(5, 10)
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2  * self.width + 2 * self.height
+    
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        else:
+            return ""
+
+    def get_amount_inside(self, rectangle):
+        rows = self.height // rectangle.height
+        cols = self.width // rectangle.width
+        return rows * cols
+
+
+a = Rectangle(5, 5)
+b = Rectangle(2, 2)
 print(a)
+print(a.get_amount_inside(b))
 
 # class Square:
