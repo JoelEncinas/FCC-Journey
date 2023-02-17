b0a876b3fa8097463332286b5f78df6bf1c658a2
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-17 15:04:16 +0100

    draw method created

--- scientific_computing_with_python/Probability_calculator/main.py
+++ scientific_computing_with_python/Probability_calculator/main.py
@@ -13,8 +13,18 @@ class Hat:
                 contents.extend(ballQ)
 
             self.contents = contents
+        
+    def draw(self, balls_to_draw):
+        if balls_to_draw > len(self.contents):
+            return self.contents
+        else:
+            balls = random.sample(self.contents, balls_to_draw)
+            for ball in balls:
+                self.contents.remove(ball)
+            return balls
 
 a = Hat(red = 2, blue = 8)
+print(a.draw(10))
 
-# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
