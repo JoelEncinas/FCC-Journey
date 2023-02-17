e57766fb880ac836dd846a223d60d130b9f2f7ea
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-17 12:39:25 +0100

    created file for the polygon area calculator

--- scientific_computing_with_python/Polygon_area_calculator/main.py
+++ scientific_computing_with_python/Polygon_area_calculator/main.py
@@ -1,1 +1,14 @@
-{\rtf1}
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    
+
+a = Rectangle(5, 10)
+print(a)
+
+# class Square:
