5e5b170261a1cdd52cddcdac451e9006be73647b
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-20 21:42:55 +0100

    fix

--- back_end_development_and_apis/Url_shortener_microservice/index.js
+++ back_end_development_and_apis/Url_shortener_microservice/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 
@@ -18,5 +18,5 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port 3000`);
 });
