82bd5da6465dbe9961d4b70a425f882081c9558c
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-20 18:52:17 +0100

    testing

--- back_end_development_and_apis/Timestamp_Microservice/index.js
+++ back_end_development_and_apis/Timestamp_Microservice/index.js
@@ -8,39 +8,62 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+function isDate(str) {
+  // match "yyyy-mm-dd" format
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  return typeof str === "string" && regex.test(str);
+}
 
-// your first API endpoint... 
-app.get("/api/:date", function (req, res) {
+// date endpoint
+app.get("/api/:date", function(req, res) {
   // get date
   const dateStr = req.params.date;
 
-  if(dateStr === "test"){
-    res.json({test: "hello express"});
+  if (dateStr === "test") {
+    res.json({ test: "hello express" });
   }
-  else {
+
+  // check  if can be parsed
+  
+  if (isDate(dateStr)) {
+    // create date
     const dateObj = new Date(dateStr);
 
     // convert to unix
     const unixDate = dateObj.getTime();
-  
+    const unixDateInt = parseInt(unixDate);
+
     // convert to utc
     const UTCDate = dateObj.toUTCString();
-    
-    res.json({unix: unixDate, utc: UTCDate});
+
+    res.json({ unix: unixDateInt, utc: UTCDate });
+  }
+  else if (!isDate(dateStr)) {
+    res.json({ error: "Invalid Date" });
+  }
+  else {
+    // create date
+    dateInt = parseInt(dateStr);
+    const dateObj = new Date(dateInt);
+
+    // convert to utc
+    const UTCDate = dateObj.toUTCString();
+
+    res.json({ unix: dateInt, utc: UTCDate });
   }
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
