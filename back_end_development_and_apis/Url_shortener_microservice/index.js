2aa646e190f01078d0ae6f3a3905406e294c2224
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-20 22:33:53 +0100

    app logic

--- back_end_development_and_apis/Url_shortener_microservice/index.js
+++ back_end_development_and_apis/Url_shortener_microservice/index.js
@@ -1,22 +1,62 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const crypto = require("crypto");
+const cors = require("cors");
+const fs = require("fs");
 const app = express();
 
 const port = 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(express.json());
+
+const filePath = "./urls.json";
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", (req, res) => {
+  const { longUrl } = req.body;
+  const shortCode = generateShortCode();
+  const urls = loadUrls();
+  urls[shortCode] = longUrl;
+  saveUrls(urls);
+  res.send({ short_url: shortCode });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port 3000`);
+app.get("/api/shorturl/:shortCode", (req, res) => {
+  const urls = loadUrls();
+  const longUrl = urls[req.params.shortCode];
+  if (longUrl) {
+    res.redirect(longUrl);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+function loadUrls() {
+  try {
+    const data = fs.readFileSync(`${__dirname}/${filePath}`);
+    return JSON.parse(data);
+  } catch (err) {
+    return {};
+  }
+}
+
+function saveUrls(urls) {
+  fs.writeFileSync(`${__dirname}/${filePath}`, JSON.stringify(urls));
+}
+
+function generateShortCode() {
+  const length = 5;
+  const buffer = crypto.randomBytes(length);
+  const code = buffer.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+  return code;
+}
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
 });
