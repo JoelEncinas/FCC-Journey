143fd7dca119d40742474978ac9351ca20ba5f09
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-21 18:11:18 +0100

    app test done

--- back_end_development_and_apis/Exercise_tracker/users.js
+++ back_end_development_and_apis/Exercise_tracker/users.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 let User = require("./userModel");
 
-router.route("/").get((req, res) => {
+router.route("/get_users").get((req, res) => {
   User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/add").post((req, res) => {
+router.route("/").post((req, res) => {
   const username = req.body.username;
 
   const newUser = new User({ username });
