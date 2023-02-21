8af385082d9e7af9f66e83ab8ce1783f271c0f96
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-21 16:21:29 +0100

    mongo setup

--- back_end_development_and_apis/Exercise_tracker/index.js
+++ back_end_development_and_apis/Exercise_tracker/index.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", true);
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("connected to db");
 });
 
-
-
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
