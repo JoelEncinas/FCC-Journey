3b7b290d11ae94f92265a2cd8b70a25bacaf6ddf
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-22 14:34:09 +0100

    format

--- back_end_development_and_apis/Exercise_tracker/index.js
+++ back_end_development_and_apis/Exercise_tracker/index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 let User = require("./userModel");
 let Exercise = require("./exerciseModel");
 
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://admin:admin@cluster0.pug2uxj.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://admin:admin@cluster0.pug2uxj.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -23,10 +24,10 @@ connection.once("open", () => {
   console.log("connected to db");
 });
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/api/users", (req, res) => {
@@ -36,15 +37,17 @@ app.post("/api/users", (req, res) => {
 
   newUser
     .save()
-    .then((savedUser) => res.json({ username: savedUser.username, _id: savedUser._id }))
+    .then((savedUser) =>
+      res.json({ username: savedUser.username, _id: savedUser._id })
+    )
     .catch((err) => res.status(400).json("Error: " + err));
-})
+});
 
 app.get("/api/users", (req, res) => {
   User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error: " + err));
-})
+});
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   User.findOne({ _id: req.params._id })
@@ -57,16 +60,30 @@ app.post("/api/users/:_id/exercises", (req, res) => {
 
       if (typeof req.body.date === "undefined" || req.body.date === "") {
         let now = new Date();
-        let dd = String(now.getDate()).padStart(2, '0');
-        let mm = String(now.getMonth() + 1).padStart(2, '0');
+        let dd = String(now.getDate()).padStart(2, "0");
+        let mm = String(now.getMonth() + 1).padStart(2, "0");
         let yyyy = now.getFullYear();
-        dateString = yyyy + '/' + mm + '/' + dd;
+        dateString = yyyy + "/" + mm + "/" + dd;
 
         const newDate = new Date(dateString);
-        date = newDate.toLocaleDateString("en-US", { weekday: "short", month: "short", day: "numeric", year: "numeric" }).replace(/,/g, '');
+        date = newDate
+          .toLocaleDateString("en-US", {
+            weekday: "short",
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+          })
+          .replace(/,/g, "");
       } else {
         const newDate = new Date(req.body.date);
-        date = newDate.toLocaleDateString("en-US", { weekday: "short", month: "short", day: "numeric", year: "numeric" }).replace(/,/g, '');
+        date = newDate
+          .toLocaleDateString("en-US", {
+            weekday: "short",
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+          })
+          .replace(/,/g, "");
       }
 
       const newExercise = new Exercise({
@@ -78,18 +95,22 @@ app.post("/api/users/:_id/exercises", (req, res) => {
 
       newExercise
         .save()
-        .then((exercise) => res.json({ _id: _id, username: username, date: exercise.date, duration: exercise.duration, description: exercise.description }))
+        .then((exercise) =>
+          res.json({
+            _id: _id,
+            username: username,
+            date: exercise.date,
+            duration: exercise.duration,
+            description: exercise.description,
+          })
+        )
         .catch((err) => res.status(400).json("Error: " + err));
     })
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-app.get("/api/users/:_id/logs", (req, res) => {
-  User.findOne({ _id: req.params._id })
-    .then
-    .catch((err) => res.status(400).json("Error: " + err));
-})
+app.get("/api/users/:_id/logs", (req, res) => {});
 
 const listener = app.listen(3000, () => {
-  console.log('Your app is listening on port 3000')
-})
+  console.log("Your app is listening on port 3000");
+});
