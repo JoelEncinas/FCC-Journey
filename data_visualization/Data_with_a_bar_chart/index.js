8e11c77522d078476a2d145bea24e48ffaf783da
JoelEncinas <joelencinasmartin@gmail.com>
2023-11-22 22:14:28 +0100

    create container

--- data_visualization/Data_with_a_bar_chart/index.js
+++ data_visualization/Data_with_a_bar_chart/index.js
@@ -2,12 +2,23 @@
 const dataURL =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
+// set up svg container dimensions
+const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+// create svg container
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 d3.json(dataURL)
   .then(function (data) {
     console.log(data);
-
-    // p with data
-    d3.select("body").append("p").text("Data loaded successfully!");
   })
   .catch(function (error) {
     console.error("Error loading data:", error);
