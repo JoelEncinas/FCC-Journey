87db4618721ae71a0fd6687df738f451388396fe
JoelEncinas <joelencinasmartin@gmail.com>
2023-11-23 15:26:36 +0100

    fetch the data using async

--- data_visualization/Data_with_a_bar_chart/index.js
+++ data_visualization/Data_with_a_bar_chart/index.js
@@ -2,9 +2,16 @@
 const dataURL =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-// set up svg container dimensions
-const margin = { top: 60, right: 60, bottom: 60, left: 60 };
-const width = 600 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+async function fetchData() {
+  try {
+    const response = await d3.json(dataURL);
+    const dataToUse = response.data;
 
+    console.log(dataToUse);
+  } catch (error) {
+    console.error("Error loading data:", error);
+  }
+}
+
+fetchData();
 
