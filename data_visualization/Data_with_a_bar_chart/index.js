d2d6424214148b1354accb5d68bc54845f81b01b
JoelEncinas <joelencinasmartin@gmail.com>
2023-11-23 15:19:43 +0100

    clear

--- data_visualization/Data_with_a_bar_chart/index.js
+++ data_visualization/Data_with_a_bar_chart/index.js
@@ -7,31 +7,4 @@ const margin = { top: 60, right: 60, bottom: 60, left: 60 };
 const width = 600 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
-// create svg container
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json(dataURL)
-  .then(function (data) {
-    let dataToUse = data.data;
-
-    console.log(dataToUse);
-
-    // parse the dates
-    var parseDate = d3.timeParse("%Y-%m-%d");
-    dataToUse.forEach(function (d) {
-      d[0] = parseDate(d[0]);
-    });
-
-    console.log(dataToUse);
-
-    
-  })
-  .catch(function (error) {
-    console.error("Error loading data:", error);
-  });
