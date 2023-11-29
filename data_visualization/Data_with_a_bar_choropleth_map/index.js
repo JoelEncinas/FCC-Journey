5daecd309057cea753fa7d98cc74f087946c5e31
JoelEncinas <joelencinasmartin@gmail.com>
2023-11-29 21:13:58 +0100

    fix data and add legend

--- data_visualization/Data_with_a_bar_choropleth_map/index.js
+++ data_visualization/Data_with_a_bar_choropleth_map/index.js
@@ -24,9 +24,9 @@ async function fetchData() {
     const fipsToDataMap = new Map(countyData.map((d) => [d.fips, d]));
 
     // Choose a color scale based on your data values
-    const colorScale = d3
-      .scaleSequential(d3.interpolateBlues)
-      .domain([3, d3.max(countyData, (d) => d.bachelorsOrHigher)]);
+    const colorScale = d3.scaleSequential(d3.interpolateBlues).domain([3, 66]);
+
+console.log(countyData);
 
     // Draw the map
     svg
@@ -34,6 +34,9 @@ async function fetchData() {
       .data(counties.features)
       .enter()
       .append("path")
+      .attr("class", "county")
+      .attr("data-fips", (d) => fipsToDataMap.get(d.id).fips)
+      .attr("data-education", (d) => fipsToDataMap.get(d.id).bachelorsOrHigher)
       .attr("d", d3.geoPath())
       .attr("fill", (d) =>
         colorScale(fipsToDataMap.get(d.id).bachelorsOrHigher)
@@ -53,14 +56,66 @@ async function fetchData() {
         .style("left", e.pageX + 10 + "px")
         .style("top", e.pageY - 30 + "px");
 
-      document.getElementById("tooltip").innerText = `${county.area_name}, ${county.state}: ${
-        county.bachelorsOrHigher
-      }%`;
+      document.getElementById(
+        "tooltip"
+      ).innerText = `${county.area_name}, ${county.state}: ${county.bachelorsOrHigher}%`;
     }
 
     function handleMouseOut(e, d) {
       tooltip.style("visibility", "hidden");
     }
+
+    // Add legend
+    const legendWidth = 300;
+    const legendHeight = 10;
+
+    const legend = svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${width - legendWidth - 20}, ${height - legendHeight - 20})`
+      );
+
+    const legendScale = d3
+      .scaleLinear()
+      .domain([3, 66])
+      .range([0, legendWidth]);
+
+    const legendAxis = d3
+      .axisBottom(legendScale)
+      .tickValues([3, 12, 21, 30, 39, 48, 57, 66])
+      .tickFormat((d) => `${d}%`);
+
+    legend
+      .append("rect")
+      .attr("id", "legend")
+      .attr("width", legendWidth)
+      .attr("height", legendHeight)
+      .style("fill", "url(#legendGradient)");
+
+    legend
+      .append("g")
+      .attr("transform", `translate(0, ${legendHeight})`)
+      .call(legendAxis);
+
+    // Add gradient to the legend
+    const defs = svg.append("defs");
+
+    const linearGradient = defs
+      .append("linearGradient")
+      .attr("id", "legendGradient")
+      .attr("x1", "0%")
+      .attr("y1", "0%")
+      .attr("x2", "100%")
+      .attr("y2", "0%");
+
+    linearGradient
+      .selectAll("stop")
+      .data(colorScale.range())
+      .enter()
+      .append("stop")
+      .attr("offset", (d, i) => i / (colorScale.range().length - 1))
+      .attr("stop-color", (d) => d);
   } catch (error) {
     console.error("Error loading data:", error);
   }
