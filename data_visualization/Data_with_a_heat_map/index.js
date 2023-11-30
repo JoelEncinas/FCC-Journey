78288dc306ee16a8180905308e01da5c689728df
JoelEncinas <joelencinasmartin@gmail.com>
2023-11-30 17:44:45 +0100

    create the map

--- data_visualization/Data_with_a_heat_map/index.js
+++ data_visualization/Data_with_a_heat_map/index.js
@@ -4,13 +4,14 @@ const dataURL =
 async function draw() {
   try {
     const response = await d3.json(dataURL);
-    const data = response;
+    const data = response.monthlyVariance;
+    const baseTemp = response.baseTemperature;
 
-    console.log(data)
+    console.log(data, baseTemp);
 
     const margin = { top: 60, right: 60, bottom: 60, left: 60 };
-    const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    const width = 1600 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
 
     const svg = d3
       .select("body")
@@ -21,6 +22,54 @@ async function draw() {
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     const tooltip = d3.select("#tooltip");
+
+    const xScale = d3
+      .scaleBand()
+      .domain(data.map((d) => d.year))
+      .range([0, width]);
+
+    const yScale = d3
+      .scaleBand()
+      .domain(data.map((d) => d.month))
+      .range([height, 0]);
+
+    const colorScale = d3
+      .scaleLinear()
+      .domain([2.8, 12.8])
+      .range([
+        "#4575B4",
+        "#74ADD1",
+        "#ABD9E9",
+        "#E0F3F8",
+        "#FFFFBF",
+        "#FEE090",
+        "#FDAE61",
+        "#F46D43",
+        "#D73027",
+      ]);
+
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .attr("id", "x-axis")
+      .call(d3.axisBottom(xScale));
+
+    svg.append("g").attr("id", "y-axis").call(d3.axisLeft(yScale));
+
+    svg
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "cell")
+      .attr("x", (d) => xScale(d.year))
+      .attr("y", (d) => yScale(d.month))
+      .attr("data-month", (d) => yScale(d.month))
+      .attr("width", xScale.bandwidth())
+      .attr("height", yScale.bandwidth())
+      .attr("fill", (d) => colorScale(baseTemp - d.variance));
+
+    const legendContainer = svg.append("g").attr("id", "legend");
   } catch (error) {
     console.error("Error", error);
   }
