c64ba8e8344d9ed33137c5b2825a5cb22dda3ec4
jencinas <joelencinasmartin@gmail.com>
2025-03-11 22:21:19 +0100

    api calls implemented

--- new_javascript_algorithms_and_data_structures/Pokemon_search_app/script.js
+++ new_javascript_algorithms_and_data_structures/Pokemon_search_app/script.js
@@ -1,3 +1,38 @@
 const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const types = document.getElementById("types");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("specialAttack");
+const specialDefense = document.getElementById("specialDefense");
+const speed = document.getElementById("speed");
 
+searchBtn.addEventListener("click", () => {
+  displayData(searchInput.value);
+});
 
+const fetchData = async (input) => {
+  let url = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${input.toLowerCase()}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+};
+
+const displayData = async (input) => {
+  const data = await fetchData(input);
+  if (data) {
+    console.log(data); // Process and update UI accordingly
+  }
+};
